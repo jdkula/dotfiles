7ccc028c90db575109dbe0de9694c80d5b97d700
Jonathan Kula <jdkula@stanford.edu>
2019-07-24 13:26:01 -0700

    Create setup-light.sh

---
+++ setup-light.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd ~
+
+sudo apt upgrade
+sudo apt install zsh wget curl python3 python3-pip python3-dev vim git zip unzip
+
+wget https://github.com/jdkula/dotfiles/archive/master.zip
+unzip master.zip
+
+cp dotfiles-master/.* .
+rm -rf dotfiles-master
+rm master.zip
+
+mv .zshrc .zshrc.bak
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+rm .zshrc
+mv .zshrc.bak .zshrc
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
