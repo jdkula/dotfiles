4d06cd2ae119abcc9621b77e2ff842e2f368e784
Jonathan Kula <jdkula@stanford.edu>
2019-08-05 22:45:06 -0700

    Update setup-light.sh

--- setup-light.sh
+++ setup-light.sh
@@ -14,18 +14,18 @@ rm master.zip
 rm .vimrc
 mv .vimrc-light .vimrc
 
-mv .zshrc .zshrc.bak
-
-rm .zshrc
-mv .zshrc.bak .zshrc
-
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 mkdir .ssh
 curl https://github.com/jdkula.keys > ~/.ssh/authorized_keys
 
+mv .zshrc .zshrc.bak
+
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+rm .zshrc
+mv .zshrc.bak .zshrc
