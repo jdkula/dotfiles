f77b25181b3523148e0c451980a50d7929ab6d22
Jonathan Kula <jdkula@stanford.edu>
2019-08-05 22:44:02 -0700

    Update setup-light.sh

--- setup-light.sh
+++ setup-light.sh
@@ -3,7 +3,7 @@
 cd ~
 
 sudo apt upgrade
-sudo apt install zsh wget curl python3 python3-pip python3-dev vim git zip unzip -y
+sudo apt install zsh wget curl python3 python3-pip vim git zip unzip -y
 
 wget https://github.com/jdkula/dotfiles/archive/master.zip
 unzip master.zip
@@ -11,13 +11,10 @@ unzip master.zip
 cp dotfiles-master/.* .
 rm -rf dotfiles-master
 rm master.zip
+rm .vimrc
+mv .vimrc-light .vimrc
 
 mv .zshrc .zshrc.bak
-
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 rm .zshrc
 mv .zshrc.bak .zshrc
@@ -27,3 +24,8 @@ vim +PluginInstall +qall
 
 mkdir .ssh
 curl https://github.com/jdkula.keys > ~/.ssh/authorized_keys
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
