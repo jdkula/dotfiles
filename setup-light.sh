953674284636c3043156c8e553268541512466e8
Jonathan Kula <jdkula@stanford.edu>
2020-03-14 13:50:13 -0700

    Update setup-light.sh
    change to unattended install

--- setup-light.sh
+++ setup-light.sh
@@ -22,7 +22,7 @@ curl https://github.com/jdkula.keys > ~/.ssh/authorized_keys
 
 mv .zshrc .zshrc.bak
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
